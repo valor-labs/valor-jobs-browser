0b1f2faef0c022f12fe556e8c3a756b880ed3ddf
Dima <tgskiv@gmail.com>
2024-07-07 00:02:00 +0300

    Better UX and some help

--- src/app/layout/top-navigation/top-navigation.component.ts
+++ src/app/layout/top-navigation/top-navigation.component.ts
@@ -24,6 +24,7 @@ export class TopNavigationComponent {
 
   @Output() menuToggled = new EventEmitter();
   noChanges: boolean = true;
+  
   jobsUrl: string = "";
   qualificationsUrl: string = "";
 
